ce6d55a0703654fee5503af61cca282be19e0c55
Peter Gumball <peter.gumball@gmail.com>
2023-04-27 08:52:39 +0200

    EP10 - Fix code again

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -8,7 +8,7 @@ pub mod systems;
 
 use crate::AppState;
 use crate::events::GameOver;
-use crate::game::systems::toggle_simulation;
+use crate::game::systems::{toggle_simulation, pause_simulation, resume_simulation};
 
 use enemy::EnemyPlugin;
 use player::PlayerPlugin;
@@ -23,12 +23,17 @@ impl Plugin for GamePlugin {
             .add_state::<SimulationState>()
             // Events
             .add_event::<GameOver>()
+            // Enter State Systems
+            .add_system(pause_simulation.in_schedule(OnEnter(AppState::Game)))
             // Plugins
             .add_plugin(EnemyPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(ScorePlugin)
             .add_plugin(StarPlugin)
-            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)))
+            // Exit State Systems
+            .add_system(resume_simulation.in_schedule(OnExit(AppState::Game)))
+        ;
     }
 }
 
