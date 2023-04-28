f7f558c8de84d8bb635738f5a019ba8156aeff00
Peter Gumball <peter.gumball@gmail.com>
2023-04-28 10:00:23 +0200

    EP10

--- src/mainmenu/mod.rs
+++ src/mainmenu/mod.rs
@@ -1,13 +1,31 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+use crate::AppState;
+use systems::layout::{despawn_main_menu, spawn_main_menu};
+
+use self::systems::interactions::{interact_with_play_button, interact_with_quit_button};
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app
+            // Enter State Systems
+            .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            // Systems
+            .add_systems(
+                (
+                    interact_with_play_button,
+                    interact_with_quit_button
+                )
+                    .in_set(OnUpdate(AppState::MainMenu))
+            )
+            // Exit State Systems
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
-}
-
-pub fn main_menu() {
 }
 
